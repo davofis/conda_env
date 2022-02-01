20773953a576c1cbe542cc61607ddadc02dead2c
davofis <davofis123@gmail.com>
2022-02-01 18:11:41 +0300

    Add azure cli

---
+++ install_azurecli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Installer for Azure-CLI
+#
+# Run: ./install_azurecli.sh
+#
+# M. Ravasi, 11/03/2021
+
+echo 'Creating Azure-CLI environment'
+
+# create conda env
+conda env create -f environment_azurecli.yml
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate azurecli
+echo 'Created and activated environment:' $(which python)
+
+echo 'Done!'
